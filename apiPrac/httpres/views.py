0010da70d55b3fc04ce895f8dee0c81b9dc28204
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-15 15:30:49 +0530

    create http's methods like get_method, post_method,put_method, patch_method, delete_method

--- apiPrac/httpres/views.py
+++ apiPrac/httpres/views.py
@@ -1,3 +1,57 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.template.context_processors import request
+from rest_framework import status
 
-# Create your views here.
+@api_view(['GET'])
+def get_method(request):
+    students = [
+        {'id': 1,
+        'name': 'suraj',},
+        {
+            'id':2,
+            'name':'irfan',
+        }
+    ]
+
+    return Response(students)
+
+@api_view(['POST'])
+def post_method(request):
+
+    data = request.data
+
+    return Response({
+        'massege': 'student add successully',
+        'data': data,
+    })
+
+@api_view(['PUT'])
+def put_method(request):
+
+    data = request.data
+
+    return Response({
+        'message': 'Data will updated',
+        'data': data
+    })
+
+@api_view(['PATCH'])
+def patch_method(request):
+    data = request.data
+
+    return Response({
+        'message': 'update data successfully',
+        'data': data
+
+    })
+
+@api_view(['DELETE'])
+def delete_method(request):
+    data = request.data
+
+    return Response({
+        'message': 'delete data successfully',
+        'data': data
+    })
