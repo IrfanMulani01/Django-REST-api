c0650090fe0b9fa9ba7cac5b5a837bc0e4400c8f
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-16 13:28:36 +0530

    create views of the get and post methods

--- apiPrac/serializer/views.py
+++ apiPrac/serializer/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializer import *
+from .models import *
 
-# Create your views here.
+@api_view(['GET'])
+def show(request):
+    student = Student.objects.all()
+
+    serializer = StudentSerializer(student, many = True)
+
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def addStudent(request):
+    serializer = StudentSerializer(data=request.data)
+    if serializer.is_valid():
+        return Response(serializer.data)
+    
