a66c8efeac048edc59f838d79801970584c190d4
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-18 13:12:28 +0530

    create new Apiview class ShowAPIView with get method

--- apiPrac/APIView/views.py
+++ apiPrac/APIView/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import *
+from .serializer import *
+
+class ShowAPIView(APIView):
+
+    def get(self, request):
+        data = {
+            'message': 'Hello API'
+        }
+        return Response(data)
