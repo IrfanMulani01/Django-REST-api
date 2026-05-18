7ce8df8c6ea0388d58c2a252f45da67f8f475837
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-18 11:39:30 +0530

    create two mwthods get and post to display all students and create new student]

--- apiPrac/modelSerializer/views.py
+++ apiPrac/modelSerializer/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import *
+from .serializer import *
 
-# Create your views here.
+@api_view(['GET'])
+def studentView(request):
+    students = Student.objects.all()
+
+    serializer = StudentModelSerializer(students, many = True)
+
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def addStudent(request):
+    serializer = StudentModelSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.error_messages)
