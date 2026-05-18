ab4a077e0b23f44804d8b1e750abfd3103d7405c
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-18 13:11:31 +0530

    create new serializer

---
+++ apiPrac/APIView/serializer.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import *
+
+class TeacherSerializer(serializers.Serializer):
+    class Meta:
+
+        model = Teacher
+
+        fields = '__all__'
