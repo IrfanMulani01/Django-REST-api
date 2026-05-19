ea6fda77c2d8349f6f5707865491cd072ca4dfaf
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-19 14:57:30 +0530

    set path this PutStud class

--- CrudAPI/crudMethods/urls.py
+++ CrudAPI/crudMethods/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('post/', StudentAdd.as_view()),
     path('get/<int:pk>/', StudetnData.as_view()),
     path('get/', ListStud.as_view()),
-    # path('put/<int:pk>/', PutStud.as_view()),
+    path('put/<int:pk>/', PutStud.as_view()),
     # path('patch/<int:pk>/', PatchStud.as_view()),
 ]
